a61deaf4c05b0b9f9a448e830064a36533b76996
Rajnish <virpaterajnish@gmail.com>
2024-07-29 11:58:26 +0530

    created a new branch for azure

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ import ffmpeg
 import subprocess
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from dotenv import load_dotenv
-from worker import process_videos
 
 load_dotenv()
 
@@ -141,8 +140,7 @@ def merge_videos_ffmpeg(video1_path, video2_path, output_path):
     combined_path = os.path.join(app.config['DOWNLOAD_FOLDER'], finalVideo)
     if os.path.exists(combined_path):
         os.remove(combined_path)
-    
-    process_videos(video1_path=video1_path,temp_video2_path=temp_video2_path,output_path=output_path)
+        
     # ffmpeg_command = [
     #     'ffmpeg',
     #     '-i', video1_path,
@@ -156,26 +154,26 @@ def merge_videos_ffmpeg(video1_path, video2_path, output_path):
     #     output_path
     # ]
 
-    # ffmpeg_command = [
-    #     'ffmpeg',
-    #     '-i', video1_path,
-    #     '-i', temp_video2_path,
-    #     '-filter_complex', (
-    #         '[0:v]scale=1080:-2[upper];'
-    #         '[1:v]scale=1080:-2[lower];'
-    #         'color=s=1080x1920:c=black[bg];'
-    #         '[bg][upper]overlay=shortest=1:y=0[bg+upper];'
-    #         '[bg+upper][lower]overlay=shortest=1:y=960'
-    #     ),
-    #     '-c:a', 'copy',
-    #     '-c:v', 'libx264',
-    #     '-preset', 'fast',
-    #     '-crf', '18',
-    #     '-t', '00:01:00',
-    #     output_path
-    # ]
-    # subprocess.run(ffmpeg_command)
-    # os.remove(temp_video2_path)
+    ffmpeg_command = [
+        'ffmpeg',
+        '-i', video1_path,
+        '-i', temp_video2_path,
+        '-filter_complex', (
+            '[0:v]scale=1080:-2[upper];'
+            '[1:v]scale=1080:-2[lower];'
+            'color=s=1080x1920:c=black[bg];'
+            '[bg][upper]overlay=shortest=1:y=0[bg+upper];'
+            '[bg+upper][lower]overlay=shortest=1:y=960'
+        ),
+        '-c:a', 'copy',
+        '-c:v', 'libx264',
+        '-preset', 'fast',
+        '-crf', '18',
+        '-t', '00:01:00',
+        output_path
+    ]
+    subprocess.run(ffmpeg_command)
+    os.remove(temp_video2_path)
 
 @app.route('/downloadmerged/<filename>')
 def download_merged(filename):
