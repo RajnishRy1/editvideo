1d6306c09df248199ce343d3a674ae6ce5e15ba1
Rajnish <virpaterajnish@gmail.com>
2024-07-31 17:11:28 +0530

    increased timeout value to complete merge process

--- startup.sh
+++ startup.sh
@@ -4,4 +4,6 @@ apt-get update
 apt-get install -y ffmpeg
 
 # Run Gunicorn to start your Flask application
-gunicorn --bind 0.0.0.0:$PORT --workers 2 app:app
+# gunicorn --bind 0.0.0.0:$PORT --workers 2 app:app
+gunicorn --bind 0.0.0.0:$PORT --workers 2 --timeout 120 app:app
+
