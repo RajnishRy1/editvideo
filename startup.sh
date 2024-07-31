00867f08bb3663094b024424e9b5e2f2e8dd3b6d
Rajnish <virpaterajnish@gmail.com>
2024-07-31 12:10:23 +0530

    added startup script to include installation of ffmpeg

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 # Update package lists and install required libraries
 apt-get update
 apt-get install -y libgl1-mesa-glx libglib2.0-0
+apt-get install -y ffmpeg
 
 # Run Gunicorn to start your Flask application
 gunicorn --bind 0.0.0.0:$PORT --workers 2 app:app
