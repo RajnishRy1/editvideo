52c194b34943c8f18c6257467449e010fbf2bba0
Rajnish <virpaterajnish@gmail.com>
2024-08-05 12:17:18 +0530

    increased workers in startup.sh

--- startup.sh
+++ startup.sh
@@ -5,5 +5,4 @@ apt-get install -y ffmpeg
 
 # Run Gunicorn to start your Flask application
 # gunicorn --bind 0.0.0.0:$PORT --workers 2 app:app
-gunicorn --bind 0.0.0.0:$PORT --workers 2 --timeout 120 app:app
-
+gunicorn --bind 0.0.0.0:$PORT --workers 4 --timeout 120 app:app
